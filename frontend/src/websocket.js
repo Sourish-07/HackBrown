34dd2512ad24aa82ced99a0f256068ec5a505d91
LaurenMBell <bell.laurenmar@gmail.com>
2026-01-31 19:14:55 -0500

    set up react server

--- frontend/src/websocket.js
+++ frontend/src/websocket.js
@@ -1,36 +1,36 @@
 export class GameWebSocket {
-    constructor(url, onMessage) {
-        this.url = url;
-        this.onMessage = onMessage;
-        this.ws = null;
+  constructor(url, onMessage) {
+    this.url = url;
+    this.onMessage = onMessage;
+    this.ws = null;
+  }
+
+  connect() {
+    this.ws = new WebSocket(this.url);
+
+    this.ws.onopen = () => {
+      console.log('Connected to Backend');
+    };
+
+    this.ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (this.onMessage) {
+        this.onMessage(data);
+      }
+    };
+
+    this.ws.onclose = () => {
+      console.log('Disconnected');
+    };
+  }
+
+  sendBet(betType) {
+    if (this.ws && this.ws.readyState === WebSocket.OPEN) {
+      this.ws.send(JSON.stringify({
+        type: 'BET',
+        payload: betType
+      }));
     }
-
-    connect() {
-        this.ws = new WebSocket(this.url);
-
-        this.ws.onopen = () => {
-            console.log('Connected to Backend');
-        };
-
-        this.ws.onmessage = (event) => {
-            const data = JSON.parse(event.data);
-            if (this.onMessage) {
-                this.onMessage(data);
-            }
-        };
-
-        this.ws.onclose = () => {
-            console.log('Disconnected');
-            // Auto reconnect logic could go here
-        };
-    }
-
-    sendBet(betType) {
-        if (this.ws && this.ws.readyState === WebSocket.OPEN) {
-            this.ws.send(JSON.stringify({
-                type: 'BET',
-                payload: betType
-            }));
-        }
-    }
+  }
 }
+
